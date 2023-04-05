f404a6f4cd65e568d91c3b0dada4749c60605532
A9vaishu <129545198+A9vaishu@users.noreply.github.com>
2023-04-05 14:39:01 +0530

    Create addition.java

---
+++ addition.java
@@ -0,0 +1,12 @@
+
+public class addition
+{
+	public static void main(String[] args) {
+	    int add=0;
+	    for(int i=1;i<=10;i++)
+	    {
+	        add=add+i;
+	    }
+		System.out.println("Addition:"+add);
+	}
+}
